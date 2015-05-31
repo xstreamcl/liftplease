3e889ffed587c01c97af0adb7ad22e91217df5c7
P K <krpiyush@gmail.com>
2015-05-31 14:25:14 +0530

    injecting bugs :)

---
+++ api/resource/user.py
@@ -0,0 +1,46 @@
+from flask_restful import fields, marshal_with, reqparse, Resource
+
+post_parser = reqparse.RequestParser()
+post_parser.add_argument(
+    'username', dest='username',
+    type=str, location='form',
+    required=True, help='The user\'s username',
+)
+post_parser.add_argument(
+    'email', dest='email',
+    type=email, location='form',
+    required=True, help='The user\'s email',
+)
+post_parser.add_argument(
+    'user_priority', dest='user_priority',
+    type=int, location='form',
+    default=1, choices=range(5), help='The user\'s priority',
+)
+
+user_fields = {
+    'id': fields.Integer,
+    'username': fields.String,
+    'email': fields.String,
+    'user_priority': fields.Integer,
+    'custom_greeting': fields.FormattedString('Hey there {username}!'),
+    'date_created': fields.DateTime,
+    'date_updated': fields.DateTime,
+    'links': fields.Nested({
+        'friends': fields.Url('user_friends'),
+        'posts': fields.Url('user_posts'),
+    }),
+}
+
+class User(Resource):
+
+    @marshal_with(user_fields)
+    def post(self):
+        args = post_parser.parse_args()
+        user = create_user(args.username, args.email, args.user_priority)
+        return user
+
+    @marshal_with(user_fields)
+    def get(self, id):
+        args = post_parser.parse_args()
+        user = fetch_user(id)
+        return user
